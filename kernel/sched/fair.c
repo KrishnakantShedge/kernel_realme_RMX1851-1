bb973c143094e110e15b8d2f21c1bd5447ed7f9f
Tyler Nijmeh <tylernij@gmail.com>
2019-05-12 21:01:55 -0700

    sched/fair: Make scheduling period more efficient
    Finish scheduling within 10 Ms (every tick for 100 Hz timers).
    Additionally, use a 1.25 Ms minimum granularity to set nr_latency to 8.
    
    Therefore, <= 8 tasks running should finish in 10 Ms. We should gain
    better battery and throughput by using this configuration.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: B0ND07 <mhashimkp@gmail.com>
    Signed-off-by: KrishnakantShedge <shedgekrishnakant33@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -84,8 +84,9 @@ walt_dec_cfs_rq_stats(struct cfs_rq *cfs_rq, struct task_struct *p) {}
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 6000000ULL;
-unsigned int normalized_sysctl_sched_latency = 6000000ULL;
+unsigned int sysctl_sched_latency = 10000000ULL;
+unsigned int normalized_sysctl_sched_latency = 10000000ULL;
+
 
 unsigned int sysctl_sched_is_big_little = 1;
 unsigned int sysctl_sched_sync_hint_enable = 1;
@@ -102,7 +103,8 @@ DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
  * SCHED_TUNABLESCALING_LINEAR - scaled linear, *ncpus
  */
 enum sched_tunable_scaling sysctl_sched_tunable_scaling
-	= SCHED_TUNABLESCALING_LOG;
+	= SCHED_TUNABLESCALING_NONE;
+
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
