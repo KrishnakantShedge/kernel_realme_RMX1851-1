40bae7b234c03a508894d2adb4ef24fb83f92457
naonana <dazumboo@gmail.com>
2020-09-21 06:41:55 +0000

    input: touchscreen: Report two different gesture input
    This is done to add support for Realme's off screen gesture implementation alongside with KharaMe Parts
    
    Signed-off-by: naonana <dazumboo@gmail.com>

--- drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
+++ drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
@@ -468,6 +468,10 @@ static void tp_gesture_handle(struct touchpanel_data *ts)
         if(ts->geature_ignore)
             return;
 #endif
+        input_report_key(ts->input_dev, KEY_F4, 1);
+        input_sync(ts->input_dev);
+        input_report_key(ts->input_dev, KEY_F4, 0);
+        input_sync(ts->input_dev);
 	if (enabled) {
             input_report_key(ts->input_dev, key, 1);
             input_sync(ts->input_dev);
