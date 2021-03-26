96fa76ca4d6b41b406930a97e2a7578def8f5092
Krishnakant Shedge <shedgekrishnakant33@gmail.com>
2021-03-26 18:50:05 +0100

    Revert "sched/fair: Add feature to toggle SchedTune utilization boosting"
    This reverts commit b80b18b8e5bb048fde0c424727219b1cb2b4fc3d.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6552,10 +6552,7 @@ boosted_cpu_util(int cpu, struct sched_walt_cpu_load *walt_load)
 
 	trace_sched_boost_cpu(cpu, util, margin);
 
-	if (sched_feat(SCHEDTUNE_BOOST_UTIL))
-		return util + margin;
-	else
-		return util;
+	return util + margin;
 }
 
 static inline unsigned long
@@ -6573,10 +6570,7 @@ boosted_task_util(struct task_struct *p)
 
 	trace_sched_boost_task(p, util, margin);
 
-	if (sched_feat(SCHEDTUNE_BOOST_UTIL))
-		return util + margin;
-	else
-		return util;
+	return util + margin;
 #endif
 }
 
