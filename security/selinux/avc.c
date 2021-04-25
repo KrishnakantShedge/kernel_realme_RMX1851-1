bb4eb78043eba29569e874469eda327a640e9ba4
Krishnakant Shedge <shedgekrishnakant33@gmail.com>
2021-04-25 21:21:57 +0200

    Revert "selinux/avc: Only log denials"
    This reverts commit 6e90f17d9ce5ed2790b0149cc855659e702f079a.

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -718,7 +718,8 @@ out:
 static void avc_audit_pre_callback(struct audit_buffer *ab, void *a)
 {
 	struct common_audit_data *ad = a;
-	audit_log_format(ab, "avc:  denied ");
+	audit_log_format(ab, "avc:  %s ",
+			 ad->selinux_audit_data->denied ? "denied" : "granted");
 	avc_dump_av(ab, ad->selinux_audit_data->tclass,
 			ad->selinux_audit_data->audited);
 	audit_log_format(ab, " for ");
@@ -737,8 +738,10 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 	avc_dump_query(ab, ad->selinux_audit_data->ssid,
 			   ad->selinux_audit_data->tsid,
 			   ad->selinux_audit_data->tclass);
-	audit_log_format(ab, " permissive=%u",
-			 ad->selinux_audit_data->result ? 0 : 1);
+	if (ad->selinux_audit_data->denied) {
+		audit_log_format(ab, " permissive=%u",
+				 ad->selinux_audit_data->result ? 0 : 1);
+	}
 }
 
 /* This is the slow part of avc audit with big stack footprint */
@@ -752,9 +755,6 @@ noinline int slow_avc_audit(u32 ssid, u32 tsid, u16 tclass,
 
 	/* Only log permissive=1 messages for SECURITY_SELINUX_DEVELOP */
 	if (denied && !result)
-		return 0;
-
-	if (!denied)
 		return 0;
 
 	if (!a) {
